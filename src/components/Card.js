14e259460f57854bf39b9d274fbea15427cca5e8
Prachi24599 <prachi24599@gmail.com>
2023-06-16 14:57:03 +0530

    react-icons package installed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Card = ({ course }) => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img src="{course.image.url}" alt="img"></img>
+        <div>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
