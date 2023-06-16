b2f172288408df9eee0e3f85862800e5d48d6503
Prachi24599 <prachi24599@gmail.com>
2023-06-16 12:42:17 +0530

    fetch data in useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
 import { apiUrl, filterData } from "./data";
 
 const App = () => {
+  const [courses, setCourses] = useState(null);
+
+  //Need to execute it first time when component is rendered, so dependency list is empty
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(apiUrl);
+      const output = await res.json();
+      //Save output into a variable
+      console.log(output);
+      setCourses(output.data);
+    };
+
+    //Calling the function
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Navbar></Navbar>
       <Filter filterData={filterData}></Filter>
-      <Cards></Cards>
+      <Cards courses={courses}></Cards>
     </div>
   );
 };
