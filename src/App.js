221cc77d52d74677794894e7102ecdde24916749
Prachi24599 <prachi24599@gmail.com>
2023-06-16 12:12:05 +0530

    Filters created using data.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Navbar from "/components/Navbar";
+import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
-import Cards from "./componentsCards";
+import Cards from "./components/Cards";
+import { apiUrl, filterData } from "./data";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Filter></Filter>
+      <Filter filterData={filterData}></Filter>
       <Cards></Cards>
     </div>
   );
