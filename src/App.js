595c6029d646ffedf0ea68d218e032e1f6de904b
Prachi24599 <prachi24599@gmail.com>
2023-06-16 10:44:17 +0530

    Compoents added in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "/components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./componentsCards";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Filter></Filter>
+      <Cards></Cards>
+    </div>
+  );
 };
 
 export default App;
