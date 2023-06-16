e777295cbccfdccb7505fbe748ab8bfcde204b73
Prachi24599 <prachi24599@gmail.com>
2023-06-16 14:55:13 +0530

    Cards component added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
 import { apiUrl, filterData } from "./data";
+import { toast } from "react-toastify";
 
 const App = () => {
   const [courses, setCourses] = useState(null);
 
   //Need to execute it first time when component is rendered, so dependency list is empty
   useEffect(() => {
+    // console.log(apiUrl);
     const fetchData = async () => {
-      const res = await fetch(apiUrl);
-      const output = await res.json();
-      //Save output into a variable
-      console.log(output);
-      setCourses(output.data);
+      try {
+        const res = await fetch(apiUrl);
+        const output = await res.json();
+        //Save output into a variable
+        console.log(output);
+        setCourses(output.data);
+      } catch (error) {
+        toast.error(error);
+      }
     };
 
     //Calling the function
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,21 +2,23 @@ import React from "react";
 import Card from "./Card";
 
 const Cards = ({ courses }) => {
+  console.log(courses);
   let allCourses = [];
 
-  const getCourses = () => {
+  //It returns a list of all courses received from API response
+  /*const getCourses = () => {
     Object.values(courses).forEach((courseCategory) => {
       courseCategory.forEach((course) => {
         allCourses.push(course);
       });
     });
     return allCourses;
-  };
+  };*/
   return (
     <div>
-      {getCourses().map((course) => {
+      {/* {getCourses().map((course) => {
         return <Card course={course} />;
-      })}
+      })} */}
     </div>
   );
 };
