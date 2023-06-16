1b0093d5f6a1463a177a32742480a0fa1978e58e
Prachi24599 <prachi24599@gmail.com>
2023-06-16 21:51:01 +0530

    showing spinner componet till get data

--- src/App.js
+++ src/App.js
@@ -2,36 +2,46 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
+import Spinner from "./components/Spinner";
 import { apiUrl, filterData } from "./data";
 import { toast } from "react-toastify";
 
 const App = () => {
   const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    //we need to show loader until we get data
+    setLoading(true);
+    try {
+      const res = await fetch(apiUrl);
+      const output = await res.json();
+      //Save output into a variable
+      console.log(output);
+      setCourses(output.data);
+    } catch (error) {
+      toast.error(error);
+    }
+    //hide loader once we get response
+    setLoading(false);
+  };
 
   //Need to execute it first time when component is rendered, so dependency list is empty
   useEffect(() => {
-    // console.log(apiUrl);
-    const fetchData = async () => {
-      try {
-        const res = await fetch(apiUrl);
-        const output = await res.json();
-        //Save output into a variable
-        console.log(output);
-        setCourses(output.data);
-      } catch (error) {
-        toast.error(error);
-      }
-    };
-
-    //Calling the function
     fetchData();
   }, []);
 
   return (
     <div>
-      <Navbar></Navbar>
-      <Filter filterData={filterData}></Filter>
-      <Cards courses={courses}></Cards>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div>
+        <Filter filterData={filterData}></Filter>
+      </div>
+      <div>
+        {setLoading ? <Spinner></Spinner> : <Cards courses={courses}></Cards>}
+      </div>
     </div>
   );
 };
